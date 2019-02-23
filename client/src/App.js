7904f21bdbbb54c9333cb2f18acdd1f4b1bf4d61
AbduQnauy <43282386+AbduQnauy@users.noreply.github.com>
2019-02-23 12:42:54 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,29 @@ import AdminPanel from './components/AdminPanel';
 
 class App extends Component {
  
+ state={
+  value:"Default",
+  selectValue:2,
+  opt:[
+    [1,"one"],
+    [2,"two"],
+    [3,"three"]
+  ]
+}; 
+ setSelectValue=(event)=>{
+  this.setState({
+    SelectValue:event.target.value
+  });
+ }
   render() {
     return (
       <Router>
         <AdminPanel path="/admin"></AdminPanel>
+        <SelectBox
+        value={this.state.selectValue} 
+        onChange={this.setSelectValue} 
+        opt={this.state.opt}
+        />
     </Router>
     );
   }
